6edf1b88762e88fa65713798aad52f22f02d0a49
Patrick Cox <patrick.r.cox@gmail.com>
2024-03-28 20:25:25 -0400

    first stuff in the hardware file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WaldonHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WaldonHardware.java
@@ -1,2 +1,8 @@
-package org.firstinspires.ftc.teamcode;public class WaldonHardware {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class WaldonHardware {
+
+    private LinearOpMode myOpMode = null;   // gain access to methods in the calling OpMode.
 }
