ebfbe3aae424eec830c0e297503b5f0d9da828f2
Patrick Cox <patrick.r.cox@gmail.com>
2024-03-28 20:54:41 -0400

    added the variables.java file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+public class Variables {
+    public static double wrist = 0;
+    public static double OutsidePixel = 0;
+    public static double InsidePixel = 0;
+    public static double Intake = 0;
+    public static double p6servo = 0;
+    public static double leftfront_drive = 0;
+    public static double leftback_drive = 0;
+    public static double rightback_drive = 0;
+    public static double rightfront_drive = 0;
+    public static double ScissorLeft = 0;
+    public static double ScissorRight = 0;
+    public static double imu = 0;
+}
